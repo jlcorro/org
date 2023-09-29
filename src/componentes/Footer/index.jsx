29e3ca6d4db2e5742ea33bdc331f6861639bf963
JairoCorro <jairolcorro@gmail.com>
2023-09-28 23:27:19 -0500

    actualización de header y footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,19 +1,19 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png)" }}>
+    return <footer className='footer' style={{ backgroundImage: "url(img/footer.png)" }}>
         <div className='redes'>
             <a href=''>
-                <img src="/img/facebook.png" alt='Facebook' />
+                <img src="img/facebook.png" alt='Facebook' />
             </a>
             <a href='https://www.linkedin.com/in/jairo-corro/'>
-                <img src="/img/likedin.png" alt='linkedin' />
+                <img src="img/likedin.png" alt='linkedin' />
             </a>
             <a href='https://github.com/jlcorro'> 
-                <img src="/img/github.png" alt='github' />
+                <img src="img/github.png" alt='github' />
             </a>
         </div>
-        <img src='/img/Logo.png' alt='org' />
+        <img src='img/Logo.png' alt='org' />
         <strong>Desarrollado por JairoCorro</strong>
     </footer>
 }
--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css"
 
 function Header() {
     return <header className="header">
-        <img src="/img/header.png" alt='Org' />
+        <img src="img/header.png" alt='Org' />
     </header>
 }
 
